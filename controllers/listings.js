e2dd59f33447af1cf63c5c48c9159a7306df2690
adarsh landge <adarsh.landge10604@gmail.com>
2024-03-23 14:35:42 +0530

    Disable Map service

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,7 @@
 const Listing=require("../models/listing.js");
-const mbxGeocoding=require("@mapbox/mapbox-sdk/services/geocoding");
-const mapToken=process.env.MAP_TOKEN;
-const geocodingClient=mbxGeocoding({accessToken : mapToken});
+// const mbxGeocoding=require("@mapbox/mapbox-sdk/services/geocoding");
+// const mapToken=process.env.MAP_TOKEN;
+// const geocodingClient=mbxGeocoding({accessToken : mapToken});
 module.exports.index=async (req, res) => {
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs", { allListings });
@@ -29,12 +29,12 @@ module.exports.showListing=async (req, res) => {
   }  
 
 module.exports.createListing=async (req, res, next) => {
-  let response=await geocodingClient
-    .forwardGeocode({
-    query: req.body.listing.location,
-    limit: 1
-  })
-    .send();
+  // let response=await geocodingClient
+  //   .forwardGeocode({
+  //   query: req.body.listing.location,
+  //   limit: 1
+  // })
+  //   .send();      doubt
     
     let url=req.file.path;
     let filename=req.file.filename;
@@ -43,7 +43,7 @@ module.exports.createListing=async (req, res, next) => {
     newListing.owner = req.user._id;
     newListing.image={url,filename};
 
-    newListing.geometry=response.body.features[0].geometry;
+    // newListing.geometry=response.body.features[0].geometry;
     
     let savedListing=await newListing.save();
     req.flash("success", "Your listing has been created!");
--- models/listing.js
+++ models/listing.js
@@ -22,17 +22,17 @@ const listingSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
-  geometry:{
-    type : {
-    type : String,
-    enum : ['Point'], 
-    required : true
-  },
-  coordinates : {
-    type : [Number],
-    required : true
-  }
-}
+//   geometry:{
+//     type : {
+//     type : String,
+//     enum : ['Point'], 
+//     required : true
+//   },
+//   coordinates : {
+//     type : [Number],
+//     required : true
+//   }
+// }
 });
 
 //mongoose middleware
